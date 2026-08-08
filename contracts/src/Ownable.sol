99c3f6a1d4f2d2b9b8b60f073d43cff0a8aad558
subheeksh5599 <komasubheeksh@gmail.com>
2026-08-08 00:43:21 +0000

    feat(contracts): two-step ownable with owner transfer

---
+++ contracts/src/Ownable.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+abstract contract Ownable {
+    address private _owner;
+    address private _pendingOwner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipTransferStarted(address indexed previousOwner, address indexed newOwner);
+
+    error OwnableUnauthorizedAccount(address account);
+    error OwnableInvalidOwner(address owner);
+
+    constructor(address initialOwner) {
+        if (initialOwner == address(0)) revert OwnableInvalidOwner(address(0));
+        _transferOwnership(initialOwner);
+    }
+
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    function pendingOwner() public view virtual returns (address) {
+        return _pendingOwner;
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        _pendingOwner = newOwner;
+        emit OwnershipTransferStarted(_owner, newOwner);
+    }
+
+    function acceptOwnership() public virtual {
+        if (msg.sender != _pendingOwner) revert OwnableUnauthorizedAccount(msg.sender);
+        _transferOwnership(_pendingOwner);
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    function _checkOwner() internal view virtual {
+        if (owner() != msg.sender) revert OwnableUnauthorizedAccount(msg.sender);
+    }
+
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
